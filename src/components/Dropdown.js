4e3f2d54da0a95595c481597f1462b3a56c3f3fb
Jade <jade.besson@hotmail.fr>
2021-06-25 14:52:18 +0200

    some styling

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,41 +1,45 @@
+import "./Dropdown.css";
+
 function Dropdown(props) {
-    const changeValue = (e) => {
-        props.setCity(e.target.value)
-        console.log(e.target.value)
-        
-    }
-    
-    const changeDeparture = (e) => {
-        props.setDeparture(e.target.value)
-        console.log(e.target.value)
-    }
-        
-    
-    return (
-        <>
-        
-        <select name="selectList" id="selectList" onChange={changeDeparture}>
-           <option value="PRG">Prague</option>
-           <option value="BER">Berlin</option>
-           <option value="WAW">Warsaw</option>
-           <option value="PED">Pardubice</option>
-        </select>
-            
-        <select name="selectList" id="selectList" onChange={changeValue}>
-           <option value="VLC">Valencia</option>
-           <option value="BCN">Barcelona</option>
-           <option value="MAD">Madrid</option>
-           <option value="MXP">Milano</option>
-           <option value="ATH">Athens</option>
-        </select>
+  const changeValue = (e) => {
+    props.setCity(e.target.value);
+    console.log(e.target.value);
+  };
 
-        </>
-            
+  const changeDeparture = (e) => {
+    props.setDeparture(e.target.value);
+    console.log(e.target.value);
+  };
 
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <select
+        className="dropdown"
+        name="selectList"
+        id="selectList"
+        onChange={changeDeparture}
+      >
+        <option value="PRG">Prague</option>
+        <option value="BER">Berlin</option>
+        <option value="WAW">Warsaw</option>
+        <option value="PED">Pardubice</option>
+      </select>
 
-        
-    )
+      <select
+        className="dropdown"
+        name="selectList"
+        id="selectList"
+        onChange={changeValue}
+      >
+        <option value="VLC">Valencia</option>
+        <option value="BCN">Barcelona</option>
+        <option value="MAD">Madrid</option>
+        <option value="MXP">Milano</option>
+        <option value="ATH">Athens</option>
+      </select>
+    </>
+  );
 }
 
 export default Dropdown;
-
--- src/components/Flight.js
+++ src/components/Flight.js
@@ -4,10 +4,18 @@ import "./Flight.css";
 function Flight(props) {
   return (
     <div className="flight">
-      <p className="city">{props.cityCodeFrom} </p>
-      <p className="time">{DateTime.fromMillis(props.dTime * 1000).toFormat("hh:mm")} </p>
-      <p className="city">{props.flyTo} </p>
-      <p className="time">{DateTime.fromMillis(props.aTime * 1000).toFormat("hh:mm")} </p>
+      <div className="departureinfo">
+        <p className="city">{props.cityCodeFrom} </p>
+        <p className="time">
+          {DateTime.fromMillis(props.dTime * 1000).toFormat("hh:mm")}{" "}
+        </p>
+      </div>
+      <div className="arrivalinfo">
+        <p className="city">{props.flyTo} </p>
+        <p className="time">
+          {DateTime.fromMillis(props.aTime * 1000).toFormat("hh:mm")}{" "}
+        </p>
+      </div>
 
       <p className="price">{props.price} EUR</p>
     </div>
