12a2f5367b05e860f1d312034826aa492abddb8a
Ungala <72914489+ungala1@users.noreply.github.com>
2021-06-25 14:29:16 +0400

    add departure into dropdown

--- src/App.js
+++ src/App.js
@@ -8,23 +8,24 @@ import Dropdown from "./components/Dropdown";
 function App() {
   const [flights, setFlights] = useState([]);
   const [city, setCity] = useState('');
+  const [departure, setDeparture] = useState('');
   const affilId = "data4youcbp202106";
   async function fetchAll() {
     const resp = await fetch(
-      `https://api.skypicker.com/flights?fly_from=PRG&fly_to=${city}&date_from=26/6/2021&date_to=29/6/2021&partner=${affilId}&limit=5&direct_flights=0`
+      `https://api.skypicker.com/flights?fly_from=${departure}&fly_to=${city}&date_from=26/6/2021&date_to=29/6/2021&partner=${affilId}&limit=5&direct_flights=0`
     );
     const data = await resp.json();
     console.log(data);
     setFlights(data.data);
   }
   useEffect(() => {
-    city && 
+    city && departure &&
     fetchAll();
-  }, [city]);
+  }, [city, departure]);
 
   return (
     <div className="App">
-      <Dropdown setCity={setCity}/>
+      <Dropdown setCity={setCity} setDeparture={setDeparture}/>
       <Flights flights={flights} />
     </div>
   );
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,16 +2,38 @@ function Dropdown(props) {
     const changeValue = (e) => {
         props.setCity(e.target.value)
         console.log(e.target.value)
-   }
+        
+    }
+    
+    const changeDeparture = (e) => {
+        props.setDeparture(e.target.value)
+        console.log(e.target.value)
+    }
+        
     
     return (
-        <select name="selectList" id="selectList" onChange={changeValue}>
-        <option value="VLC">Valencia</option>
-        <option value="BCN">Barcelona</option>
-        <option value="MAD">Madrid</option>
-        <option value="MXP">Milano</option>
-        <option value="ATH">Athens</option>
+        <>
+        
+        <select name="selectList" id="selectList" onChange={changeDeparture}>
+           <option value="PRG">Prague</option>
+           <option value="BER">Berlin</option>
+           <option value="WAW">Warsaw</option>
+           <option value="PED">Pardubice</option>
         </select>
+            
+        <select name="selectList" id="selectList" onChange={changeValue}>
+           <option value="VLC">Valencia</option>
+           <option value="BCN">Barcelona</option>
+           <option value="MAD">Madrid</option>
+           <option value="MXP">Milano</option>
+           <option value="ATH">Athens</option>
+        </select>
+
+        </>
+            
+
+
+        
     )
 }
 
